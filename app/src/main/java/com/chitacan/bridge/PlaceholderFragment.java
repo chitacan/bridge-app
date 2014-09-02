c4e3f675cfbda8dc6deff14c800fb78f204bee66
kyung yeol kim <chitacan@gmail.com>
2014-09-02 17:21:40 +0900

    add socket.io-client.java to application.

--- app/src/main/java/com/chitacan/bridge/PlaceholderFragment.java
+++ app/src/main/java/com/chitacan/bridge/PlaceholderFragment.java
@@ -16,16 +16,37 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.CancelledKeyException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.UnresolvedAddressException;
+import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment implements View.OnClickListener{
 
-    private Extender mExtender   = null;
+    private ServerBridge mServer = null;
+    private DaemonBridge mDaemon = null;
+
     private TextView mStatus     = null;
 
     private EditText mLocalPortNumber  = null;
-    private EditText mRemotePortNumber = null;
+    private EditText mRemoteAddr       = null;
 
     private Handler mHandler = new Handler() {
 
@@ -63,7 +84,7 @@ public class PlaceholderFragment extends Fragment implements View.OnClickListene
         rootView.findViewById(R.id.btn_start).setOnClickListener(this);
         rootView.findViewById(R.id.btn_stop) .setOnClickListener(this);
         mStatus = (TextView) rootView.findViewById(R.id.section_status);
-        mRemotePortNumber = (EditText) rootView.findViewById(R.id.edit_remote_port);
+        mRemoteAddr = (EditText) rootView.findViewById(R.id.edit_remote_addr);
         mLocalPortNumber  = (EditText) rootView.findViewById(R.id.edit_local_port);
         return rootView;
     }
@@ -79,35 +100,297 @@ public class PlaceholderFragment extends Fragment implements View.OnClickListene
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_start:
-                startExtender();
+                startBridge();
                 break;
             case R.id.btn_stop:
-                stopExtender();
+                stopBridge();
                 break;
         }
     }
 
-    private void stopExtender() {
-        if (mExtender != null)
-            mExtender.interrupt();
-        mExtender = null;
+    private void stopBridge() {
+        if (mDaemon != null)
+            mDaemon.interrupt();
+        mDaemon = null;
+        mServer.disconnect();
     }
 
-    private void startExtender() {
-        String remotePort = mRemotePortNumber.getText().toString();
+    private void startBridge() {
+        String remoteAddr = mRemoteAddr.getText().toString();
         String localPort  = mLocalPortNumber .getText().toString();
 
-        if (remotePort.length() == 0 || localPort.length() == 0) {
+        if (remoteAddr.length() == 0 || localPort.length() == 0) {
             Toast.makeText(getActivity(), "no remote port number", Toast.LENGTH_LONG).show();
             return;
         }
-        if (mExtender == null || !mExtender.isAlive()) {
-            mExtender = new Extender(
-                    "127.0.0.1",    Integer.parseInt(localPort),
-                    "redribbon.io", Integer.parseInt(remotePort)
-            );
-            mExtender.setHandler(mHandler);
-            mExtender.start();
+
+        if (mServer == null) {
+            mServer = new ServerBridge(remoteAddr);
+            mServer.subscribe();
+        }
+
+        if (mDaemon == null || !mDaemon.isAlive()) {
+            mDaemon = new DaemonBridge();
+        }
+
+        mServer.setDaemon(mDaemon);
+        mDaemon.setServer(mServer);
+        mDaemon.setHandler(mHandler);
+        mServer.setHandler(mHandler);
+        mDaemon.start();
+        mServer.connect();
+    }
+
+    class ServerBridge {
+
+        private Socket mSocket = null;
+        private boolean isConnected = false;
+        private DaemonBridge mDaemon = null;
+        private Handler mStatusHandler = null;
+
+        public ServerBridge(String url) {
+            IO.Options opt = new IO.Options();
+            opt.reconnectionDelay    = 10 * 1000;
+            opt.reconnectionDelayMax = 20 * 1000;
+
+            try {
+                mSocket = IO.socket(url, opt);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void subscribe() {
+            mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    isConnected = true;
+                    setStatus("server connected");
+                }
+
+            }).on("data", new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    mDaemon.queue(args[0]);
+                }
+
+            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    isConnected = false;
+                    String reason = (String) args[0];
+                    setStatus("server disconnected - " + reason);
+                }
+
+            }).on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    Exception e = (Exception) args[0];
+                    setStatus("server connect error - " + e.getMessage());
+                }
+
+            }).on(Socket.EVENT_CONNECT_TIMEOUT, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    setStatus("server timeout");
+                }
+            }).on(Socket.EVENT_RECONNECTING, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    setStatus("server reconnecting...");
+                }
+            });
+        }
+
+        public Socket getSocket() {
+            return mSocket;
+        }
+
+        public Socket connect() {
+            return mSocket.connect();
+        }
+
+        public void disconnect() {
+            mSocket.disconnect();
+        }
+
+        public void setDaemon(DaemonBridge bridge) {
+            mDaemon = bridge;
+        }
+
+        public boolean isConnected() {
+            return isConnected;
+        }
+
+        public void setHandler(Handler handler) {
+            mStatusHandler = handler;
+        }
+
+        private void setStatus(String status) {
+            if (mStatusHandler == null) return;
+
+            Message msg = mStatusHandler.obtainMessage();
+            msg.what = 0;
+            msg.obj = status;
+            mStatusHandler.sendMessage(msg);
+        }
+    }
+
+    class DaemonBridge extends Thread {
+        private Selector mSelector = null;
+        private ServerBridge mServer = null;
+        private ArrayBlockingQueue mQueue = new ArrayBlockingQueue(10);
+
+        private SocketChannel channel = null;
+
+        // setup buffers.
+        private ByteBuffer mLBuffer = ByteBuffer.allocateDirect(6 * 1024);
+        private ByteBuffer mRBuffer = ByteBuffer.allocateDirect(6 * 1024);
+
+        private InetSocketAddress mLAddr = null;
+        private Handler mStatusHandler = null;
+
+        DaemonBridge() {
+            this.setName("Daemon");
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            String result = null;
+            try {
+                init();
+                connect();
+                loop();
+            } catch (ConnectException e) {
+                // adbd connect exception
+                result = "adbd is not available";
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (CancelledKeyException e) {
+                e.printStackTrace();
+            } catch (UnresolvedAddressException e) {
+                // no connectiom. (airplane mode)
+                e.printStackTrace();
+            } catch (JSONException e) {
+                result = "JSON parse exception";
+                e.printStackTrace();
+            } finally {
+                close(result);
+            }
+        }
+
+        private void init() throws IOException {
+            mLAddr = new InetSocketAddress("127.0.0.1", 6666);
+            mSelector = Selector.open();
+            channel = SocketChannel.open();
+            channel.configureBlocking(false);
+            channel.register(mSelector, SelectionKey.OP_CONNECT);
+        }
+
+        private void connect() throws IOException {
+            channel.connect(mLAddr);
+        }
+
+        private void loop() throws IOException, JSONException {
+            setStatus("daemon loop");
+            while(true) {
+                if (this.isInterrupted()) break;
+
+                int ready = mSelector.select();
+
+                if (mQueue.peek() != null) {
+                    JSONObject obj = (JSONObject) mQueue.poll();
+                    byte[] data = (byte[]) obj.get("binary");
+                    mRBuffer.put(data);
+                    mRBuffer.flip();
+                    int write = channel.write(mRBuffer);
+                    setStatus("write : " + write);
+                    mRBuffer.clear();
+                }
+
+                if (ready == 0) continue;
+
+                Iterator<SelectionKey> i = mSelector.selectedKeys().iterator();
+
+                while(i.hasNext()) {
+                    SelectionKey key = i.next();
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    if (key.isConnectable()) {
+                        if (sc.isConnectionPending()) {
+                            sc.finishConnect();
+                            setStatus("daemon connected");
+                            key.interestOps(SelectionKey.OP_READ);
+                        }
+                    } else if (key.isReadable()) {
+                        int read = sc.read(mLBuffer);
+                        setStatus("read : " + read);
+                        if (read == -1)
+                            return;
+                        else {
+                            mLBuffer.flip();
+                            byte[] b = new byte[mLBuffer.remaining()];
+                            mLBuffer.get(b);
+                            mLBuffer.clear();
+                            JSONObject req = new JSONObject();
+                            req.put("hello", "req");
+                            req.put("binary", b);
+                            mServer.getSocket().emit("res", req);
+                        }
+                    }
+                    i.remove();
+                }
+            }
+        }
+
+        private void close(String msg) {
+            if (msg == null)
+                msg = "daemon close";
+            try {
+                if (channel != null)
+                    channel.close();
+
+                mQueue.clear();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            setStatus(msg);
+        }
+
+        public boolean queue(Object obj) {
+            boolean result = mQueue.offer(obj);
+            mSelector.wakeup();
+            return result;
+        }
+
+        public void setServer(ServerBridge server) {
+            mServer = server;
+        }
+
+        @Override
+        public void interrupt() {
+            mSelector.wakeup();
+            super.interrupt();
+        }
+
+        public void setHandler(Handler handler) {
+            mStatusHandler = handler;
+        }
+
+        private void setStatus(String status) {
+            if (mStatusHandler == null) return;
+
+            Message msg = mStatusHandler.obtainMessage();
+            msg.what = 0;
+            msg.obj = status;
+            mStatusHandler.sendMessage(msg);
         }
     }
 }
