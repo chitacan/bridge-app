4f9fd0ca512d5803d1e8812f8eb7c1c2f7a06e39
kyung yeol kim <chitacan@gmail.com>
2014-10-10 14:58:31 +0900

    [app] add utility method for logging.

--- app/src/main/java/com/chitacan/bridge/Util.java
+++ app/src/main/java/com/chitacan/bridge/Util.java
@@ -7,6 +7,17 @@ import java.net.URL;
  * Created by chitacan on 2014. 9. 25..
  */
 public class Util {
+    static final String TAG = "chitacan";
+
+    static final String createTag() {
+        return createTag(null);
+    }
+
+    static final String createTag(String localTag) {
+        String seprator = localTag == null ? "" : ":";
+        return new StringBuffer(TAG).append(seprator).append(localTag).toString();
+    }
+
     static String createUrl(String host, int port, String path) {
         if (path == null) path = "";
         try {
